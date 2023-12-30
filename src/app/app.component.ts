10aad019d6b9d2c8dcecdaef40a746edd78961e4
Ali Keshvari <alikeshvari1400@gmail.com>
2023-12-30 22:35:08 +0330

    add json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,23 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   nameInput: string = '';
   dateInput: string = '';
-
+  amountInput: string = '';
+  heightInput: number = 0;
+  car = {
+    model : "BMW",
+    year : 2018,
+    type : "s500"
+  }
   onNameChange(value: string) {
     this.nameInput = value;
   }
-  onDateChange(value: string){
+  onDateChange(value: string) {
     this.dateInput = value;
+  }
+  onAmountChange(value: string) {
+    this.amountInput = value;
+  }
+  onHeightChange(value: number) {
+    this.heightInput = value;
   }
 }
